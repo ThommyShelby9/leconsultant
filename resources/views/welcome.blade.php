003eac395e9d6f1a127e21aff210a45286ad1403
ThommyShelby9 <merluxsolitaire@gmail.com>
2024-09-30 13:42:41 +0100

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
                 html: `<kkiapay-widget 
                     amount="1490" 
                     key="2a9363b7c6c78cf76717f8895a561990f39bac73" 
-                    data="5"
+                    data="1"
                     callback="{{ route('pack.payant' , 10 ) }} "
                     sandbox="false" 
                </kkiapay-widget>`,
